13ce66acb55a7a72db1791b9bcf8ba116d11b9dc
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-17 19:29:24 +0100

    Added TypeOrmModule for: Invoice and Invoice Detail

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Invoice } from './entities/invoice.entity';
+import { InvoiceDetail } from './entities/invoice-detail.entity';
 import { InvoiceService } from './invoice.service';
 import { InvoiceController } from './invoice.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Invoice, InvoiceDetail])],
   controllers: [InvoiceController],
-  providers: [InvoiceService]
+  providers: [InvoiceService],
 })
 export class InvoiceModule {}
