aba856e6c1c4122633619429ee4cbc3ecf9517b2
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-17 19:26:51 +0100

    Updated DTOs for update invoice

--- src/invoice/dto/update-invoice.dto.ts
+++ src/invoice/dto/update-invoice.dto.ts
@@ -1,4 +1,39 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInvoiceDto } from './create-invoice.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsPositive, IsDate, IsString, IsNumber, Min, IsNotEmpty, IsArray, IsInt, IsUUID, ValidateNested, IsOptional} from 'class-validator';
+import { InvoiceDetailDto } from './invoice-detail.dto';
 
-export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {}
+export class UpdateInvoiceDto {
+  @ApiProperty({
+    description: 'Invoice Date',
+  })
+  @IsOptional()
+  @IsDate()
+  date: Date;
+
+  @ApiProperty({
+    description: 'Customer Name',
+  })
+  @IsOptional()
+  @IsString()
+  @Min(5)
+  customerName: string;
+
+  @ApiProperty({
+    description: 'Customer Address',
+  })
+  @IsOptional()
+  @IsString()
+  @Min(5)
+  customerAddress: string;
+
+  @ApiProperty({
+    type: InvoiceDetailDto,
+    isArray: true,
+  })
+  @ValidateNested()
+  @IsArray()
+  @Type(() => InvoiceDetailDto)
+  @IsOptional()
+  details: InvoiceDetailDto[];
+}
