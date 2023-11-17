e5735f70b534540823fc5fced1baf5719395f688
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-17 19:24:46 +0100

    Added dotenv, swagger documentation, and config build for api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { config } from 'dotenv';
+config(); // Load environment variables from .env file
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('ACME Startup Service')
+    .setDescription('The ACME Startup Service API Documentation')
+    .setVersion('1.0')
+    .addTag('invoices')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
