0b4d943c21a3f93089da1d0902876ab06fe3fd1a
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-17 19:23:18 +0100

    Added | Updated TypeOrmModule with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [InvoiceModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Change to your database type (e.g., 'mysql')
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'username',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_DATABASE || 'acme_startup_app_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+    InvoiceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
