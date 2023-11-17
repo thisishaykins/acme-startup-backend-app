dc6abbb566ced63003135e2e0d6694560d5bf365
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-17 19:35:15 +0100

    Implementation of Invoices Services to handle all CRUD Operations...

--- src/invoice/invoice.service.ts
+++ src/invoice/invoice.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Invoice } from './entities/invoice.entity';
+import { InvoiceDetail } from './entities/invoice-detail.entity';
 import { CreateInvoiceDto } from './dto/create-invoice.dto';
 import { UpdateInvoiceDto } from './dto/update-invoice.dto';
 
 @Injectable()
 export class InvoiceService {
-  create(createInvoiceDto: CreateInvoiceDto) {
-    return 'This action adds a new invoice';
+  constructor(
+    @InjectRepository(Invoice)
+    private readonly invoiceRepository: Repository<Invoice>,
+  ) {}
+
+  async create(invoice: Invoice) {
+    return await this.invoiceRepository.save(invoice);
   }
 
-  findAll() {
-    return `This action returns all invoice`;
+  async findAll() {
+    const result = await this.invoiceRepository.find();
+    return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} invoice`;
+  async findOne(id: number) {
+    return this.invoiceRepository.findOneBy({
+      id,
+    });
   }
 
-  update(id: number, updateInvoiceDto: UpdateInvoiceDto) {
-    return `This action updates a #${id} invoice`;
+  async update(id: number, invoice: Invoice) {
+    const hasInvoice = await this.invoiceRepository.findOneBy({id});
+    if (!hasInvoice) throw new Error('Invoice does not exist...');
+
+    await this.invoiceRepository.update(id, invoice);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} invoice`;
+  async remove(id: number) {
+    const hasInvoice = await this.invoiceRepository.findOneBy({ id });
+    if (!hasInvoice) throw new Error('Invoice does not exist...');
+
+    await this.invoiceRepository.delete(id);
   }
 }
