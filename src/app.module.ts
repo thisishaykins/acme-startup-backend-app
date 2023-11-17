6d6955369c1296bcee1ef0802a253e4cedfd6bc1
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-11-17 01:55:06 +0100

    Added the invoice resource - base app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [],
+  imports: [InvoiceModule],
   controllers: [AppController],
   providers: [AppService],
 })
